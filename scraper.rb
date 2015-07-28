a73c2c463901c8945f4030c518547f3aa91a1cae
Tony Bowden <tony@tmtm.com>
2015-07-28 11:13:57 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,11 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'wikidata/fetcher'
+
+WikiData::Category.new('Category:UK MPs 2015–20').wikidata_ids.each do |id|
+  data = WikiData::Fetcher.new(id: id).data or next
+  puts data
+  ScraperWiki.save_sqlite([:id], data)
+end
