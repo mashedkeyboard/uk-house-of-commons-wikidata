fccf52dd6b60042d1fb17a2a371204fcc1dcafaa
Tony Bowden <tony@tmtm.com>
2015-10-16 17:48:32 +0100

    Get member information directly from Wikidata

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,24 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'colorize'
+require 'nokogiri'
+require 'pry'
 require 'scraperwiki'
 require 'wikidata/fetcher'
 
-WikiData::Category.new('Category:UK MPs 2015–20').wikidata_ids.each do |id|
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+def ids_from_claim(claim_str)
+  url = 'https://wdq.wmflabs.org/api?q=claim[%s]' % claim_str
+  json = JSON.parse(open(url).read, symbolize_names: true)
+  json[:items].map { |id| "Q#{id}" }
+end
+
+wd_ids = ids_from_claim('463:21084473')
+
+wd_ids.each do |id|
   data = WikiData::Fetcher.new(id: id).data or next
   ScraperWiki.save_sqlite([:id], data)
 end
