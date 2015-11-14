df263bbe25c467f1788591895025eb978618b803
Tony Bowden <tony@tmtm.com>
2015-11-14 12:36:07 +0000

    Ping when done

--- scraper.rb
+++ scraper.rb
@@ -6,6 +6,7 @@ require 'nokogiri'
 require 'pry'
 require 'scraperwiki'
 require 'wikidata/fetcher'
+require 'rest-client'
 
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
@@ -38,3 +39,6 @@ wd_ids.each_with_index do |id, i|
   data = WikiData::Fetcher.new(id: id).data or next
   ScraperWiki.save_sqlite([:id], data)
 end
+
+warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+
