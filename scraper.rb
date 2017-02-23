aa4d0651ad4f93462e1e074b122104e17cbb0953
Tony Bowden <tony@tmtm.com>
2017-02-23 15:48:20 +0000

    Also scrape P39s with Legislative Period qualifier
    We're moving from people being 'Member of: <term>" to having the term
    set as a qualifier on their legislative membership. This makes sure we
    get people from both for now.

--- scraper.rb
+++ scraper.rb
@@ -3,22 +3,33 @@
 
 require 'wikidata/fetcher'
 
-terms = { 
+terms = {
   56 => '21084473',
   55 => '21084472',
   54 => '21084471',
   53 => '21084470',
   52 => '21084469',
-  # 51 => '21084468',
-  # 50 => '21084467',
-  # 49 => '21084466',
-  # 48 => '21084465',
-  # 47 => '21084464',
-  # 46 => '21084463',
 }
 
-sparq = 'SELECT ?item WHERE { ?item wdt:P463 wd:Q%s . }'
-ids = terms.values.map { |t| EveryPolitician::Wikidata.sparql(sparq % t) }.reduce(:|)
+# TODO: make this a single query that includes all the terms we care about
+qual_sparq = <<EOQ
+  SELECT ?item
+  WHERE {
+    ?item p:P39 ?position_statement .
+    ?position_statement ps:P39 wd:Q16707842 ;
+                        pq:P2937 wd:Q%s .
+  }
+EOQ
+
+# People with a position 'UK MP', with 'legislative period: @term' qualifier
+qual_ids = terms.values.map { |t| EveryPolitician::Wikidata.sparql(qual_sparq % t) }.reduce(:|)
+
+# People with a 'member of: @term'
+term_sparq = 'SELECT ?item WHERE { ?item wdt:P463 wd:Q%s . }'
+term_ids = terms.values.map { |t| EveryPolitician::Wikidata.sparql(term_sparq % t) }.reduce(:|)
+
+
+ids = qual_ids | term_ids
 warn "To fetch: #{ids.count}"
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, batch_size: 50, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, batch_size: 50)
