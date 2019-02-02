327e955f16ab3fee1674c7d008446d912f94e336
Tony Bowden <tony@tmtm.com>
2019-02-02 08:02:33 +0000

    Only fetch members of the 52nd Parliament onwards

--- scraper.rb
+++ scraper.rb
@@ -5,7 +5,11 @@ require 'wikidata/fetcher'
 
 # People with a position 'UK MP', of a suitable subclass
 qual_sparq = <<EOQ
-  SELECT DISTINCT ?item WHERE { ?item p:P39 [ ps:P39 [ wdt:P279 wd:Q16707842 ] ] }
+  SELECT DISTINCT ?item WHERE {
+    ?item p:P39/ps:P39 [ wdt:P279 wd:Q16707842 ; wdt:P2937 ?term ] .
+    ?term p:P31/pq:P1545 ?ordinal .
+    FILTER (xsd:integer(?ordinal) >= 52)
+  }
 EOQ
 qual_ids = EveryPolitician::Wikidata.sparql(qual_sparq)
 
